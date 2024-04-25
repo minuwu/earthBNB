110e9aa97d3242b4eb7352ee246901634605fb78
minuwu <84508269+minuwu@users.noreply.github.com>
2024-04-25 09:22:23 +0600

    creating & using templates/ layouts/ partials

--- app.js
+++ app.js
@@ -5,10 +5,15 @@ const PORT = 8080;
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust"
 const Listing = require("./models/listing.js")
 const path = require("path");
+const methodOverride = require("method-override");
+const ejsMate = require("ejs-mate");
 
 app.set("view engine","ejs");
 // app.set("views",path.join(__dirname,"views"));
 app.use(express.urlencoded({extended: true}));
+app.use(methodOverride("_method"));
+app.use(express.static(path.join(__dirname,"public")));
+app.engine("ejs", ejsMate);
 
 
 main()
@@ -48,7 +53,7 @@ app.get("/listings",async (req,res)=>{
     console.log("GET: /listings RESPONDED ");
     
 })
-//create route: 
+//new route: 
 app.get("/listings/new",(req,res)=>{
     res.render("listings/new.ejs");
 })
@@ -58,6 +63,7 @@ app.get("/listings/:id", async (req,res)=>{
     let listing = await Listing.findById(id);
     res.render("listings/show.ejs", {listing});
 })
+//create route: 
 app.post("/listings",async (req,res)=>{
     let {title, description, image, price, location, country} = req.body;
     let newListing = new Listing({
@@ -71,7 +77,25 @@ app.post("/listings",async (req,res)=>{
     let result = await newListing.save();
     res.redirect("/listings");
 })
-
+//edit route:
+app.get("/listings/:id/edit",async (req,res)=>{
+    let {id}  = req.params;
+    let listing = await Listing.findById(id);
+    res.render("listings/edit.ejs", {listing});
+})
+//update route:
+app.put("/listings/:id", async (req,res)=>{
+    let {id} = req.params;
+    await Listing.findByIdAndUpdate(id,{...req.body.listing});
+    res.redirect(`/listings/${id}`);
+})
+//delete route:
+app.delete("/listings/:id", async (req,res)=>{
+    let {id} = req.params;
+    let deletedListing = await Listing.findByIdAndDelete(id);
+    console.log(deletedListing);
+    res.redirect("/listings");
+})
 app.listen(PORT,(req,res)=>{
     console.log("Express: App listening on port: ",PORT);
 })
