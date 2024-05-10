d543fc92ee68e1c4e9954be07e62765ea98ba89d
minuwu <84508269+minuwu@users.noreply.github.com>
2024-05-10 08:51:32 +0600

    styling based on user logged status

--- middleware.js
+++ middleware.js
@@ -1,7 +1,7 @@
 module.exports.isLoggedIn = (req, res, next) =>{
     if(!req.isAuthenticated()){
         req.flash("error","Log In is required");
-        return res.redirct("/login");
+        return res.redirect("/login");
     }
     next();
 }
